8265a8e6dd9bd965f3b45cb0ff865734b23a469d
bhaskar-S04 <sarenbhaskar328@gmail.com>
2025-08-11 11:05:13 +0530

    Initial Commit

--- App.js
+++ App.js
@@ -1,34 +1,52 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, TouchableOpacity, Button, Vibration, StyleSheet, TextInput } from "react-native";
+import { View, Text, TouchableOpacity, Button, StyleSheet, TextInput } from "react-native";
+import {useAudioPlayer} from "expo-audio";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
   const [count, setCount] = useState(0);
-  const [target, setTarget] = useState(108);
+  const [target, setTarget] = useState(0);
   const [tempTarget, setTempTarget] = useState("");
+  
+  const player = useAudioPlayer(require('./assets/beep-125033.mp3'));
+
 
   useEffect(() => {
     const loadData = async () => {
+    try{
       const savedCount = await AsyncStorage.getItem("count");
       const savedTarget = await AsyncStorage.getItem("target");
-      if (savedCount) setCount(parseInt(savedCount));
-      if (savedTarget) setTarget(parseInt(savedTarget));
+      if (savedCount !== null) setCount(parseInt(savedCount));
+      if (savedTarget !== null) setTarget(parseInt(savedTarget));
+      } catch (error) {
+      console.log("Error loading data:", error);
+      }
     };
     loadData();
   }, []);
 
+
   useEffect(() => {
     AsyncStorage.setItem("count", count.toString());
-    AsyncStorage.setItem("target", target.toString());
-  }, [count, target]);
+  }, [count]);
 
-  const increment = () => {
+  useEffect(()=>{
+    if (target > 0) {
+      AsyncStorage.setItem("target", target.toString());
+    }
+  }, [target]);
+
+
+    const increment = () => {
     const newCount = count + 1;
     setCount(newCount);
-    if (newCount === target) {
-      Vibration.vibrate();
+    if (newCount === target && player) {
+      player.seekTo(0);
+      player.play();
     }
   };
+
+  
 
   return (
     <View style={styles.container}>
@@ -57,21 +75,10 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#fff",
-  },
+  container: {flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "white"},
   counter: { fontSize: 40, marginBottom: 30 },
-  bigButton: { backgroundColor: "#ff9800", padding: 40, borderRadius: 100 },
-  buttonText: { fontSize: 24, color: "#fff" },
+  bigButton: { backgroundColor: "orange", padding: 40, borderRadius: 100 },
+  buttonText: { fontSize: 24, color: "white" },
   row: { flexDirection: "row", marginTop: 20, alignItems: "center" },
-  input: {
-    borderWidth: 1,
-    borderColor: "#ccc",
-    padding: 10,
-    width: 100,
-    marginHorizontal: 10,
-  },
+  input: { borderWidth: 1, borderColor: "white", padding: 10, width: 100, marginHorizontal: 10},
 });
